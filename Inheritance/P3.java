d29455e5f23eb398d16b6071c3e69707c6073808
manojrajm <gauthamtamizha007@gmail.com>
2024-08-02 13:11:01 +0530

    file changes

--- Inheritance/P3.java
+++ Inheritance/P3.java
@@ -1,29 +1,40 @@
-//Create a class called "Shape" with properties such as "numSides" and "area". Create a subclass called "Rectangle" that inherits from the Shape class and adds properties such as "length" and "width".
-class Shape
-{
-	int numsSides;
-	int area;
-	Shape(int numsSides,int area)
-	{
-		this.area=area;
-		this.numSides=numSides;
-	}
+class Shape {
+    int numSides;  
+    int area;
+
+    Shape(int numSides, int area) {
+        this.numSides = numSides;
+        this.area = area;
+    }
 }
-class Rectangle extends Shape
-{
-	int length;
-	int width;
-	Rectangle(int numsSides,int area,int length,int width)
-	{
-		super(area,numSides);
-		this.length=length;
-		this.width=width;
-	}
+
+class Rectangle extends Shape {
+    int length;
+    int width;
+
+    Rectangle(int numSides, int area, int length, int width) {
+        super(numSides, area); 
+        this.length = length;
+        this.width = width;
+    }
+
+    public void calculateArea() {
+        this.area = length * width;
+    }
+
+    public void display() {
+        System.out.println("Number of Sides: " + numSides);
+        System.out.println("Length: " + length);
+        System.out.println("Width: " + width);
+        System.out.println("Area: " + area);
+    }
 }
-public class P3
-{
-	public static void main(String[] args)
-	{
-		Rectangle rec=new Rectangle(4,);
-	}
+
+public class P3 {
+    public static void main(String[] args) {
+        Rectangle rec = new Rectangle(4, 0, 5, 3); 
+        rec.calculateArea();
+        rec.display();
+    }
 }
+
